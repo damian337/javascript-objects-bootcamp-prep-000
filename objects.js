d4fd2a4fb21ab742302e0bf9079a4b0c7223d5ea
Damian Guillory <guillorywdamian@gmail.com>
2017-10-05 01:03:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(object, key, value) {
     object[key] = value
     return object
 }
+
+function removeFromPlaylist(object, key)  {
+    delete object.key
+    return object
+}
